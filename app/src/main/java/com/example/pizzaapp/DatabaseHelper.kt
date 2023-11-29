d1e50037cbbfd909648d619551918ed0306caeac
asthiviaamitaba <asthiviaoktandaamitaba@students.amikom.ac.id>
2023-11-29 16:17:00 +0700

    initial commit

--- app/src/main/java/com/example/pizzaapp/DatabaseHelper.kt
+++ app/src/main/java/com/example/pizzaapp/DatabaseHelper.kt
@@ -1,11 +1,14 @@
 package com.example.pizzaapp
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Toast
 
-class DatabaseHelper(context: Context): SQLiteOpenHelper (
+class DatabaseHelper(var context: Context): SQLiteOpenHelper (
     context, DATABASE_NAME, null, DATABASE_VERSION
         ){
     companion object {
@@ -40,28 +43,69 @@ class DatabaseHelper(context: Context): SQLiteOpenHelper (
     }
 
     fun checkLogin(email:String, password:String):Boolean{
-        val colums = arrayOf(COLUMN_NAME)
+        val columns = arrayOf(COLUMN_NAME)
         val db = this.readableDatabase
-        //selection criteria
         val selection = "$COLUMN_EMAIL = ? AND $COLUMN_PASSWORD = ?"
-        //selection arguments
         val selectionArgs = arrayOf(email, password)
 
-        val cursor = db.query(TABLE_ACCOUNT, //table to query
-            colums, //columns to return
+        val cursor = db.query(TABLE_ACCOUNT,
+            columns, //columns to return
             selection, //columns for WHERE clause
             selectionArgs, //the values for the WHERE clause
-            null, //group the rows
-            null, //filter by row groups
-            null) //the sort order
+            null, // group by the rows
+            null, // filter by row groups
+            null) // the sort order
         val cursorCount = cursor.count
         cursor.close()
         db.close()
 
-        //check data available or not
-        if (cursorCount > 0)
+        if(cursorCount > 0)
             return true
         else
             return false
+    }
+
+    fun addAccount(email:String, name:String, level:String, password: String){
+        val db = this.readableDatabase
+
+        val values = ContentValues()
+        values.put(COLUMN_EMAIL, email)
+        values.put(COLUMN_NAME, name)
+        values.put(COLUMN_LEVEL, level)
+        values.put(COLUMN_PASSWORD, password)
+
+        val result = db.insert(TABLE_ACCOUNT, null, values)
+
+        if (result==(0).toLong()){
+            Toast.makeText(context, "Register Failed", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(context, "Register Success, " +
+            "please login using your new account", Toast.LENGTH_SHORT).show()
+        }
+        db.close()
+    }
+
+    @SuppressLint("Range")
+    fun checkData(email:String):String{
+        val colums = arrayOf(COLUMN_NAME)
+        val db = this.readableDatabase
+        val selection = "$COLUMN_EMAIL = ? "
+        val selectionArgs = arrayOf(email)
+        var name : String = ""
+
+        val cursor = db.query(TABLE_ACCOUNT,
+            colums,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            null)
+        if(cursor.moveToFirst()){
+            name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME))
+        }
+        cursor.close()
+        db.close()
+        return name
     }
 }
--- app/src/main/java/com/example/pizzaapp/LoginActivity.kt
+++ app/src/main/java/com/example/pizzaapp/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.example.pizzaapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -19,11 +23,45 @@ class LoginActivity : AppCompatActivity() {
         val txtPassword:EditText = findViewById(R.id.editTextPassword)
         //instance button login
         val btnLogin:Button = findViewById(R.id.buttonLogin)
+        val tvRegis:TextView = findViewById(R.id.buttonRegister)
 
         //event button login
         btnLogin.setOnClickListener {
-            val intentLogin = Intent(this@LoginActivity, MainActivity::class.java)
-            startActivity(intentLogin)
+            val databaseHelper = DatabaseHelper(this)
+
+            val data:String = databaseHelper.checkData("asthiviaoktandaamitaba@students.amikom.ac.id")
+            Toast.makeText(this@LoginActivity, "Result : " + data,
+                Toast.LENGTH_SHORT).show()
+            if(data == ""){
+                databaseHelper.addAccount("asthiviaoktandaamitaba@students.amikom.ac.id",
+                "Asthivia Oktanda A", "Cashier", "amikom")
+            }
+            val email = txtUsername.text.toString().trim()
+            val password = txtPassword.text.toString().trim()
+
+            val result:Boolean = databaseHelper.checkLogin(email, password)
+            if (result==true){
+                Toast.makeText(this@LoginActivity, "Login Success",
+                    Toast.LENGTH_SHORT).show()
+                val intentLogin = Intent(this@LoginActivity,
+                    MainActivity::class.java)
+                startActivity(intentLogin)
+            } else {
+                Toast.makeText(this@LoginActivity, "Login Failed, Try Again !!!",
+                    Toast.LENGTH_SHORT).show()
+                txtUsername.hint = "username"
+                txtPassword.hint = "password"
+                txtUsername.isFocused
+
+            }
+
+        }
+
+        tvRegis.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
+
+
 }
